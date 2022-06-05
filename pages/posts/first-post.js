c52a46d9f19bee6a028c751a2987c65d459bdb25
Mark <mark@remarkablemark.org>
2022-06-04 20:06:38 -0400

    feat(pages): add profile image to first post
    https://nextjs.org/learn/basics/assets-metadata-css/assets

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 export default function FirstPost() {
@@ -9,6 +10,13 @@ export default function FirstPost() {
           <a>Back to home</a>
         </Link>
       </h2>
+
+      <Image
+        src="/images/profile.jpeg" // Route of the image file
+        height={144} // Desired size with correct aspect ratio
+        width={144} // Desired size with correct aspect ratio
+        alt="Your Name"
+      />
     </>
   )
 }
