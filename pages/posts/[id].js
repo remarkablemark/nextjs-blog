d43a86fb80214ebf830df183bca69e782fc0ee93
Mark <mark@remarkablemark.org>
2022-06-04 23:38:09 -0400

    feat(posts): polish the post page
    https://nextjs.org/learn/basics/dynamic-routes/polishing-post-page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,16 +1,23 @@
+import Head from 'next/head'
+
 import Layout from '../../components/layout'
+import Date from '../../components/date'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import utilStyles from '../../styles/utils.module.css'
 
 export default function Post(props) {
   return (
     <Layout>
-      {props.postData.title}
-      <br />
-      {props.postData.id}
-      <br />
-      {props.postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: props.postData.contentHtml }} />
+      <Head>
+        <title>{props.postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{props.postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={props.postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: props.postData.contentHtml }} />
+      </article>
     </Layout>
   )
 }
