64b24202c9c398c695682d7834bcf6415d1904a4
Mark <mark@remarkablemark.org>
2022-06-04 21:12:01 -0400

    feat: polish layout
    https://nextjs.org/learn/basics/assets-metadata-css/polishing-layout

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Link from 'next/link'
 import Script from 'next/script'
 
@@ -11,7 +10,6 @@ export default function FirstPost() {
       <Head>
         <title>First Post</title>
       </Head>
-
       <Script
         src="https://unpkg.com/github-corners/dist/embed.min.js"
         strategy="lazyOnload"
@@ -19,21 +17,12 @@ export default function FirstPost() {
         data-href="https://github.com/remarkablemark/nextjs-blog"
         data-target="_blank"
       />
-
       <h1>First Post</h1>
-
       <h2>
         <Link href="/">
           <a>Back to home</a>
         </Link>
       </h2>
-
-      <Image
-        src="/images/profile.jpeg" // Route of the image file
-        height={144} // Desired size with correct aspect ratio
-        width={144} // Desired size with correct aspect ratio
-        alt="Your Name"
-      />
     </Layout>
   )
 }
