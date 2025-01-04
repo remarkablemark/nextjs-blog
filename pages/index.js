1ad0892a4e6b56fd97fdb089f26e9e0e0cfc2e9d
Mark <mark@remarkablemark.org>
2025-01-03 21:26:56 -0500

    fix: fix dependencies and Next.js error
    https://nextjs.org/docs/messages/invalid-new-link-with-extra-anchor

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ export default function Home(props) {
           {props.allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
               <Link href={`/posts/${id}`}>
-                <a>{title}</a>
+                {title}
               </Link>
               <br />
               <small className={utilStyles.lightText}>
